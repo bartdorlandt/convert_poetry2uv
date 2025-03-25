5c6128276016e3ec97688c56248d8c116061ea1f
Bart Dorlandt <bart@bamweb.nl>
2025-03-25 20:47:55 +0100

    Improved email mathing (#15)
    * Added task entries for bump
    
    * Bump version: 0.1.11 → 0.1.12

--- convert_poetry2uv.py
+++ convert_poetry2uv.py
@@ -42,8 +42,10 @@ def version_conversion(version: str) -> str:
 
 def authors_maintainers(new_toml: tk.TOMLDocument) -> None:
     project = new_toml["project"]
-    user_email = re.compile(r"^([\w, ]+) <([\-\w@.]+)>$")
-    only_email = re.compile(r"^<([\-\w@.]+)>$")
+    user_email = re.compile(
+        r"^([\w, ]+) <([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)>$"
+    )
+    only_email = re.compile(r"^<([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)>$")
     only_user = re.compile(r"^([\w, ]+)$")
 
     for key in ("authors", "maintainers"):
